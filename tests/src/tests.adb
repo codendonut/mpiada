006075f7c59deb9d1052cd24162296899cf9ba3a
krispyensign <futralj@gmail.com>
2025-08-07 00:06:22 -0400

    improved tests

--- tests/src/tests.adb
+++ tests/src/tests.adb
@@ -2,17 +2,29 @@ with Ada.Text_IO;
 with MPI_Ada;
 
 procedure Tests is
-   world_comm : MPI_Ada.MPI_Comm;
+   world_comm : constant MPI_Ada.MPI_Comm := MPI_Ada.Init;
+   local_rank : Natural := 0;
 begin
-   world_comm := MPI_Ada.Init;
-   pragma Assert (world_comm.Size = 1);
-   pragma Assert (world_comm.Rank = 0);
+   Ada.Text_IO.Put_Line ("Init - SUCCESS");
+   pragma Assert (world_comm.Rank >= 0 and then world_comm.Rank <= 2);
+   local_rank := world_comm.Rank;
+   Ada.Text_IO.Put_Line ("Rank" & local_rank'Image & " - SUCCESS");
+
+   pragma Assert (world_comm.Size >= 1 and then world_comm.Size <= 3);
+   if local_rank = 0 then
+      Ada.Text_IO.Put_Line ("Size - SUCCESS");
+   end if;
 
    world_comm.Barrier;
    pragma Assert (True);
+   if local_rank = 0 then
+      Ada.Text_IO.Put_Line ("Barrier - SUCCESS");
+   end if;
 
    MPI_Ada.Finalize;
    pragma Assert (True);
+   if local_rank = 0 then
+      Ada.Text_IO.Put_Line ("Finalize - SUCCESS");
+   end if;
 
-   Ada.Text_IO.Put_Line ("SUCCESS");
 end Tests;
