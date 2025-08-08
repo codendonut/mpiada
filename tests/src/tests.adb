dbcdf565837551046b1a751eb7faa85443a9b0e3
krispyensign <futralj@gmail.com>
2025-08-08 16:03:21 -0400

    add vendor specific file

--- tests/src/tests.adb
+++ tests/src/tests.adb
@@ -2,15 +2,20 @@ with Ada.Text_IO;
 with MPI_Ada;
 
 procedure Tests is
-   world_comm : constant MPI_Ada.MPI_Comm := MPI_Ada.Init;
+   world_comm : MPI_Ada.MPI_Comm;
    local_rank : Natural := 0;
+   local_size : Natural := 0;
 begin
+   MPI_Ada.Init ("tests");
+   world_comm := MPI_Ada.Get_Comm_World;
+
    Ada.Text_IO.Put_Line ("Init - SUCCESS");
-   pragma Assert (world_comm.Rank >= 0 and then world_comm.Rank <= 2);
    local_rank := world_comm.Rank;
+   pragma Assert (local_rank >= 0 and then local_rank <= 2);
    Ada.Text_IO.Put_Line ("Rank" & local_rank'Image & " - SUCCESS");
 
-   pragma Assert (world_comm.Size >= 1 and then world_comm.Size <= 3);
+   local_size := world_comm.Size;
+   pragma Assert (local_size >= 1 and then local_size <= 3);
    if local_rank = 0 then
       Ada.Text_IO.Put_Line ("Size - SUCCESS");
    end if;
