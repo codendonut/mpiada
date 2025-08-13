b9ac6561887ffc0ce41053a88718a7d9369d9394
krispyensign <futralj@gmail.com>
2025-08-13 09:51:22 -0400

    remove opinionated versions

--- src/mpi_ada-generic_ops.adb
+++ src/mpi_ada-generic_ops.adb
@@ -18,24 +18,6 @@ package body MPI_Ada.Generic_Ops is
            comm_handle);
    end MPI_Send;
 
-   function MPI_Send
-     (comm_handle : API.MPI_Comm_Handle;
-      buf         : T;
-      count       : Interfaces.C.int;
-      data_type   : API.MPI_Datatype_Handle;
-      dest_rank   : Interfaces.C.int;
-      message_tag : Interfaces.C.int) return Integer is
-   begin
-      return
-        Funcs.MPI_Send
-          (API.Message_Addr (buf'Address),
-           count,
-           data_type,
-           dest_rank,
-           message_tag,
-           comm_handle);
-   end MPI_Send;
-
    function MPI_Recv
      (buf_out     : out T;
       count       : Interfaces.C.int;
@@ -56,23 +38,4 @@ package body MPI_Ada.Generic_Ops is
            API.MPI_Status_Addr (status_out'Address));
    end MPI_Recv;
 
-   function MPI_Recv
-     (comm_handle : API.MPI_Comm_Handle;
-      buf_out     : out T;
-      count       : Interfaces.C.int;
-      data_type   : API.MPI_Datatype_Handle;
-      source_rank : Interfaces.C.int;
-      message_tag : Interfaces.C.int;
-      status_out  : out MPI_Status) return Integer is
-   begin
-      return
-        Funcs.MPI_Recv
-          (API.Message_Addr (buf_out'Address),
-           count,
-           data_type,
-           source_rank,
-           message_tag,
-           comm_handle,
-           API.MPI_Status_Addr (status_out'Address));
-   end MPI_Recv;
 end MPI_Ada.Generic_Ops;
