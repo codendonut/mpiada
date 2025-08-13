55ff339d57b0a675d436384d497226581bbf4e35
krispyensign <futralj@gmail.com>
2025-08-13 10:01:28 -0400

    add opinionated wrappers

--- src/mpi_ada.ads
+++ src/mpi_ada.ads
@@ -15,16 +15,27 @@ package MPI_Ada is
    with Convention => C;
 
    function MPI_Init (program_name : String) return Integer;
+   function Init (program_name : String) return Integer renames MPI_Init;
 
    function MPI_Finalize return Integer renames Funcs.MPI_Finalize;
+   function Finalize return Integer renames Funcs.MPI_Finalize;
 
    function MPI_Comm_size
      (comm : API.MPI_Comm_Handle; size : out Integer) return Integer;
+   function Size
+     (comm : API.MPI_Comm_Handle; size : out Integer) return Integer renames
+      MPI_Comm_size;
 
    function MPI_Comm_rank
      (comm : API.MPI_Comm_Handle; rank : out Integer) return Integer;
+   function Rank
+     (comm : API.MPI_Comm_Handle; rank : out Integer) return Integer renames
+     MPI_Comm_rank;
 
    function MPI_Barrier (comm_addr_in : API.MPI_Comm_Handle) return Integer
+   renames Funcs.MPI_Barrier;
+
+   function Barrier (comm_addr_in : API.MPI_Comm_Handle) return Integer
    renames Funcs.MPI_Barrier;
 
 end MPI_Ada;
