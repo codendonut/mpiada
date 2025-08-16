4001c00440a9415660fbdf0e51ec7c84e2ac1aae
krispyensign <futralj@gmail.com>
2025-08-15 21:44:36 -0400

    remove opinionated functions

--- src/mpi_ada-generic_ops.ads
+++ src/mpi_ada-generic_ops.ads
@@ -3,6 +3,7 @@ generic
    type Index is range <>;
    type T is array (Index range <>) of Element;
 package MPI_Ada.Generic_Ops is
+
    function MPI_Send
      (buf         : T;
       count       : Interfaces.C.int;
@@ -11,14 +12,6 @@ package MPI_Ada.Generic_Ops is
       message_tag : Interfaces.C.int;
       comm_handle : API.MPI_Comm_Handle) return Integer;
 
-   function Send
-     (comm_handle : API.MPI_Comm_Handle;
-      buf         : T;
-      count       : Interfaces.C.int;
-      data_type   : API.MPI_Datatype_Handle;
-      dest_rank   : Interfaces.C.int;
-      message_tag : Interfaces.C.int) return Integer;
-
    function MPI_Recv
      (buf_out     : out T;
       count       : Interfaces.C.int;
@@ -26,15 +19,6 @@ package MPI_Ada.Generic_Ops is
       source_rank : Interfaces.C.int;
       message_tag : Interfaces.C.int;
       comm_handle : API.MPI_Comm_Handle;
-      status_out  : out MPI_Status) return Integer;
-
-   function Recv
-     (comm_handle : API.MPI_Comm_Handle;
-      buf_out     : out T;
-      count       : Interfaces.C.int;
-      data_type   : API.MPI_Datatype_Handle;
-      source_rank : Interfaces.C.int;
-      message_tag : Interfaces.C.int;
       status_out  : out MPI_Status) return Integer;
 
 end MPI_Ada.Generic_Ops;
